c68336934acadf5d1f750376ea9a867a191c7aa3
mate0227 <kissmate0227@gmail.com>
2023-12-06 12:39:34 +0100

    controllers
    controllers added + test razor page

---
+++ konyvtar/Controllers/BooksController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace konyvtar.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly IBookService _bookService;
+
+        public BooksController(IBookService bookService)
+        {
+            _bookService = bookService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Book>>> GetBooks()
+        {
+            var books = await _bookService.Get();
+            return Ok(books);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Book>> GetBook(int id)
+        {
+            var book = await _bookService.Get(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            return Ok(book);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Book>> AddBook(Book book)
+        {
+            await _bookService.Add(book);
+            return CreatedAtAction(nameof(GetBook), new { id = book.Id }, book);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Book>> UpdateBook(int id, Book book)
+        {
+            if (id != book.Id)
+            {
+                return BadRequest();
+            }
+
+            await _bookService.Update(book);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Book>> DeleteBook(int id)
+        {
+            var book = await _bookService.Get(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            await _bookService.Delete(book);
+            return Ok(book);
+        }
+    }
+}
---
+++ konyvtar/Controllers/ReadersController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace konyvtar.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReadersController : ControllerBase
+    {
+        private readonly IReaderService _readerService;
+
+        public ReadersController(IReaderService readerService)
+        {
+            _readerService = readerService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Reader>>> GetReaders()
+        {
+            var readers = await _readerService.Get();
+            return Ok(readers);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Reader>> GetReader(int id)
+        {
+            var reader = await _readerService.Get(id);
+            if (reader == null)
+            {
+                return NotFound();
+            }
+            return Ok(reader);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Reader>> AddReader(Reader reader)
+        {
+            await _readerService.Add(reader);
+            return CreatedAtAction(nameof(GetReader), new { id = reader.Id }, reader);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Reader>> UpdateReader(int id, Reader reader)
+        {
+            if (id != reader.Id)
+            {
+                return BadRequest();
+            }
+
+            await _readerService.Update(reader);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Reader>> DeleteReader(int id)
+        {
+            var reader = await _readerService.Get(id);
+            if (reader == null)
+            {
+                return NotFound();
+            }
+
+            await _readerService.Delete(reader);
+            return Ok(reader);
+        }
+    }
+}
